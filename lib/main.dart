1ce1f22d6d29a68628ef7330dc8e3a75fb6d9c47
alonecheer <alonecheer@hotmail.com>
2020-07-19 20:48:52 +0700

    import font

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //final wordPair = WordPair.random();  // Delete this line
-
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          //child: Text(wordPair.asPascalCase), // Change the highlighted text to...
-          child: RandomWords(), // ... this highlighted text
-        ),
-      ),
+      home: Text('Hello'),
     );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
   }
 }
