19167006e9beb51237aef508f327505018189a9f
alonecheer <alonecheer@hotmail.com>
2020-07-19 23:40:44 +0700

    Container medical

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Widget dictionary() {
+    return Container(
+      height: 230,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            height: 50,
+            child: Icon(
+              Icons.menu,
+              size: 50,
+              color: Colors.white,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: 100,
+            child: Text(
+              "DICTIONARY",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget medical() {
+    return Container(
+      padding: const EdgeInsets.only(left: 40.0),
+      alignment: Alignment.center,
+      height: 230,
+      decoration: BoxDecoration(border: Border.all()),
+      child: Text(
+        "MEDICAL TERMINOLOGY",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 35,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(1, 91, 100, 1),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: dictionary(),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: medical(),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
